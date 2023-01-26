366af1f12ecddd85d63357c1a61675156f43b64e
godwin kondo <kondogodwin@gmail.com>
2023-01-26 23:52:26 +0000

    3rd commit

--- 03data_data_types.py
+++ 03data_data_types.py
@@ -3,10 +3,13 @@
 # String -- str
 # String is a collection of characters declared within a single or double quotation mark
 
+Name= 'Daniel'
+print(type(Name))
 
 # Integer -- int
 # Integer is a positive or negative whole number
-
+firstNumber= 2
+print(type(firstNumber))
 
 # Float -- float
 # A float is a fractional or decimal value
@@ -14,7 +17,13 @@
 
 # Complex -- complex
 # A complex number is a number which consist of a real part and an imaginary part.
+myValue= 2 + 5j
+print(myValue)
 
+print(type(myValue), end="\n \n")
 
 # Boolean -- bool
 # A boolean data type represents data that can only exist in two forms.
+isRegistered= True
+print(isRegistered)
+print(type(isRegistered))
