e177eba9fe3910ba3841545c9b388c493f6e6066
godwin kondo <kondogodwin@gmail.com>
2023-01-27 13:17:25 +0000

    3rd commit

--- 03data_data_types.py
+++ 03data_data_types.py
@@ -3,18 +3,29 @@
 # String -- str
 # String is a collection of characters declared within a single or double quotation mark
 
+Name= 'Daniel'
+print(type(Name))
 
 # Integer -- int
 # Integer is a positive or negative whole number
-
+firstNumber= 2
+print(type(firstNumber))
 
 # Float -- float
 # A float is a fractional or decimal value
-
+number1= 2.534
+print(number1)
+print(type(number1))
 
 # Complex -- complex
 # A complex number is a number which consist of a real part and an imaginary part.
+myValue= 2 + 5j
+print(myValue)
 
+print(type(myValue), end="\n \n")
 
 # Boolean -- bool
 # A boolean data type represents data that can only exist in two forms.
+isRegistered= True
+print(isRegistered)
+print(type(isRegistered))
--- 05dynamic_vs_static_prog_lang.py
+++ 05dynamic_vs_static_prog_lang.py
@@ -1,4 +1,4 @@
 # Characteristics of Python programming language
 # Dynamic vs Static Programming Languages
 # Interpreted vs Compiled languages
-# High level vs Low level languages 
+# High level vs Low level languages
