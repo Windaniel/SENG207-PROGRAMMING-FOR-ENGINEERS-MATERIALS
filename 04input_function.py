d35e4513a0c8a2c9b38db854c1009ac70070954a
godwin kondo <kondogodwin@gmail.com>
2023-02-03 16:06:08 +0000

    03/02/23 Commit

--- 04input_function.py
+++ 04input_function.py
@@ -1,6 +1,7 @@
 # Python input function
 # modules are files in the system that are hiding
 from getpass import getpass
+import maskpass #to mask the password 
 
 # name= input("Enter your Name :")
 # print("your name is :", name)
@@ -8,6 +9,9 @@ from getpass import getpass
 # print("your pswd is: ", password)
 
 #using the module getpass
-password = getpass("Enter your password")
-print("your password is: ", password)
+# password = getpass("Enter your password")
+# print("your password is: ", password)
 
+
+pswd= maskpass.askpass("enter your password: ")
+print(pswd)
