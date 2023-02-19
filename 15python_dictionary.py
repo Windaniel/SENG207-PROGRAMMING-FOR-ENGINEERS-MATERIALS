0946d53e612bc380340e21706151c460e4369914
godwin kondo <kondogodwin@gmail.com>
2023-02-19 22:43:23 +0000

    19/02/2023

--- 15python_dictionary.py
+++ 15python_dictionary.py
@@ -2,6 +2,22 @@
 # It is ordered and mutable.
 # It allows duplicate members
 
+student_info = {
+    'name': 'Godwin',# key 'name' : value of the key 
+    'age' : 19,
+    'email':'danielkofi@gmail.com',
+    'ID': '10950054',
+} 
+student_info['name'] = 'Daniel'
+print(student_info['name']) # print(Dictionary_Name['keyName'])
+print(type(student_info))
+
+fruit = ['mango', 'apple', 'orange']
+
+fruit[1] = 'pineaple'
+print(fruit)
+# python dictionary are ordered by the keys
+
 # Dictionary Methods
 
 # Copy -- Returns a copy of the dictionary
--- 16loop.py
+++ 16loop.py
@@ -1,6 +1,29 @@
 # Loop
 # For loop
 
+# number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# total= 0 
+# for x in number:
+#     total += x
+# print(total)
+
+# Assignment
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# total= 1 
+# for x in numbers :
+#     total *= x     
+# print(total)
+
+# Zip method
+names= ['kelly', 'fafa', 'john', 'sika']
+scores = [10, 12, 56, 75] 
+for name, score in zip(names , scores):
+    print(f'{name} score: {score}')
+
+# range Method  
+for x in range(1990, 2023,4) :
+    print(f'{x} was a voting year')
+    
 # While loop
 
 # Loop through List
@@ -15,4 +38,4 @@
 
 # Exercise
 # 1. Find all the even numbers in a range
-# 2. Find the sum of all elements in a list
+# 2. Find the factorial of a number
