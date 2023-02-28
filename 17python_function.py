5169eabc72dca5c5160f0b2d07c4b67dfa62a3df
godwin kondo <kondogodwin@gmail.com>
2023-02-28 14:46:44 +0000

    Work Done

--- 17python_function.py
+++ 17python_function.py
@@ -1,13 +1,28 @@
 # A function is a block of reusable code 
 # Place a parenthesis() in front of the function name to call / invoke it
 
+# def myFunction():
+#     return 'this is my function'
+# print(myFunction())
+
+
 # Parameters vs Argument
 # A parameter is the variable listed inside the parentheses in the function definition.
 # An argument is the value that is sent to the function when it is called.
 
+# def myFunction(name, age):
+#     return f"my name is {name} I'm {age} years old"
+
+# print(myFunction('godwin',20))
+# print(age = 12, name= 'Godwin') # explicite form 
 
 # Functions with Parameters and Argument
+def addition(x ,y):
+    return x + y 
 
+print(addition(3, 8))
+
+x, *y = 2,5,6,8,7,6
 
 # Important to be mindful of indentation
 
