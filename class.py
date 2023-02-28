e6c46907f2da49c8c583d459d426e418b9ff6643
godwin kondo <kondogodwin@gmail.com>
2023-02-28 14:46:19 +0000

    work Done on 28/2/2023

---
+++ class.py
@@ -0,0 +1,30 @@
+class bakingPan:
+    department= 'Materials Science and Engineering' #class variable
+    unite_price= 6
+    def __init__(self, flour, sugar, special_ingredian):
+        self.flour= flour # instance.attribute
+        self.sugar= sugar
+        self.special_ingredient= special_ingredian
+
+    def bread_name(self):
+        return f'{self.special_ingredient} bread'
+    
+    def total_price(self,quantity):
+        total = quantity * self.unite_price
+        return f'the total price of {quantity} {self.bread_name()} = {total} Ghc'
+
+
+
+bread1= bakingPan('soft',20,'wheat') # object = class(argument)
+bread2= bakingPan('Hard',10,'coconut')
+
+
+print(bread1.flour)
+print(bread2.flour) 
+print(bread2.department) #accessing through the object 
+print(bakingPan.department) #accessing the class variable
+print(bread1.bread_name()) #object.method 
+print(bread2.bread_name()) #object.method 
+print(bread1.total_price(10)) #object.method 
+print(bread2.total_price(5)) #object.method 
+ 
---
+++ practice.py
@@ -0,0 +1,39 @@
+#python OOP
+#Defining a class 
+# class bakingPan:
+#     pass 
+# Defining an object of the class 
+# Note that an object is an instance of a class
+# bread = bakingPan()
+###############################################################
+###############################################################
+
+# Defining an atribute of the object bread 
+# bread.flour = 'Rose' #same as flour= 'Rose'
+# bread.sugar = 20
+# bread.special_ingredient = 'Wheat' 
+###############################################################
+###############################################################
+# Defining a Method
+# class bakinPan():
+#     def myFunction():
+#         print('this is my first method')
+
+# print(bakingPan.myFunction())
+
+#Defining a Constructor or __init_ Method 
+# In this case the object become a instance of the method __init_
+# Self is representing the object or instance of the class __init_
+# class bakingPan():
+#     def __init__(self, floor, sugar, special_ingrediant):
+#         self.floor = floor
+#         self.sugar= sugar 
+#         self.special_ingredient= special_ingrediant
+
+# bread = bakingPan('soft', 20, 'Wheat')
+# print(bread.floor)
+
+###############################################################
+###############################################################
+
+# accessing a variable within a class 
